4411ac4858bac650b75535bb1e92be4e6c226ba3
cag2050 <cag2050@192.168.0.101>
2022-07-17 13:28:47 +0800

    缩进加4个空格约束

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // 行尾不需要加分号
+    semi: false,
+    // 末尾不需要加逗号
+    trailingComma: "none",
+    //  If you want Prettier to force all prose blocks to be on a single line and rely on editor/viewer soft wrapping instead, you can use "never"
+    proseWrap: "never",
+    // "ignore" - Whitespace (or the lack of it) around all tags is considered insignificant
+    htmlWhitespaceSensitivity: "ignore",
+    // false - Do not enforce single attribute per line
+    singleAttributePerLine: false,
+    //每行最多多少个字符换行
+    printWidth: 300
+}
